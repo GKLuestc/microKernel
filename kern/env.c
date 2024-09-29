fff17c52178cff9dd2afb073e16dae49c5186f1e
GKL <271730475@qq.com>
2024-09-29 13:44:39 +0800

    update

--- kern/env.c
+++ kern/env.c
@@ -428,6 +428,8 @@ env_create(uint8_t *binary, enum EnvType type)
 
 	// If this is the file server (type == ENV_TYPE_FS) give it I/O privileges.
 	// LAB 5: Your code here.
+
+	
 	struct Env *env;
 	
 	// 申请一个空闲环境，父进程设置为 0 
